5e3c1f744bc3a3dd8f200ddcff25ffdf37d8edb1
tathagatamishra <tathagatamishra007@gmail.com>
2022-12-19 13:44:19 +0530

    V_1.0.0 --- Base structer created

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = require('./route/route')
+
+const app = express()
+
+app.use(express.json())
+
+app.use('/', router)
+
+
+app.listen(3000, () => console.log('server is 🏃 🏃 🏃'))
---
+++ src/route/route.js
@@ -0,0 +1,31 @@
+const express = require('express')
+
+const router = express.Router()
+
+
+
+//! User APIs ------
+router.post  ('/')
+router.post  ('/')
+router.get   ('/')
+router.put   ('/')
+
+//! Products APIs ------
+router.post  ('/')
+router.get   ('/')
+router.get   ('/')
+router.put   ('/')
+router.delete('/')
+
+//! Cart APIs ------
+router.post  ('/')
+router.put   ('/')
+router.get   ('/')
+router.delete('/')
+
+//! Checkout/Order APIs ------
+router.post  ('/')
+router.put   ('/')
+
+
+module.exports = router
